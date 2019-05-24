a92a3b2dd5d41567eea0c625729358c655575521
SANTOSHVA <50978914+SANTOSHVA@users.noreply.github.com>
2019-05-24 12:02:17 +0530

    Create hi.c

---
+++ hi.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+        int sum,a,b;
+        printf("enter two numbers\n");
+        scanf("%d%d",&a,&b);
+        sum=a+b;
+        printf("the sum of two number is %d",sum);
+        return 0;
+}

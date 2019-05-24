d10d46454cc83f5bc4b9f97bce17c49b8e19e061
SANTOSHVA <50978914+SANTOSHVA@users.noreply.github.com>
2019-05-24 15:20:37 +0530

    Create add2num.c

---
+++ add2num.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int input(int *a, int *b)
+{
+printf("Enter 2 num \n");
+scanf("%d%d",a,b);
+    printf("Enter 2 num \n");
+    scanf("%d%d",a,b);
+}
+int add(int x, int y)
+{
+int s;
+s=x+y;
+return s;
+}
+int output(int m)
+{
+printf("ans is = %d",m);
+    printf("ans is = %d",m);
+}
+int main()
+{
+int a,b,s;
+input(&a,&b);
+s=add(a,b);
+output(s);
+}
